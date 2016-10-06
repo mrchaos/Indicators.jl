01e074446c3ec501d0cc97b170b164ed54b73ad6
Jacob Amos <jamos125@gmail.com>
2016-10-06 02:14:19 -0500

    added crossover/under functions

--- src/utils.jl
+++ src/utils.jl
@@ -17,7 +17,7 @@ end
 @doc doc"""
 crossunder{Float64}(x::Vector{Float64}, y::Vector{Float64})
 
-Find where `x` crosses over `y` (returns boolean vector where crossunder occurs)
+Find where `x` crosses under `y` (returns boolean vector where crossunder occurs)
 """ ->
 function crossunder{Float64}(x::Vector{Float64}, y::Vector{Float64})
     @assert size(x,1) == size(y,1)
