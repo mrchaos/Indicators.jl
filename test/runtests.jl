6ac0ffc175e43cc0c3653237034a0bbbb43083ec
dysonance <jamos125@gmail.com>
2017-10-12 21:14:27 -0400

    not using csv files for testing anymore -- more ma test cases

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,20 +1,51 @@
 using Indicators
+using Temporal
 using Base.Test
 
-# Read sample data and calculations
-const tol = 1e-8
-data = X = readcsv("AAPL-2015.csv", header=true)[1]
-ma = readcsv("ma.csv", header=true)[1]
-o = data[:,1]
-h = data[:,2]
-l = data[:,3]
-c = x = data[:,4]
-v = data[:,5]
-hlc = data[:,2:4]
-ohlc = data[:,1:4]
+const n = 1e3
+const x0 = 50.0
 
-# Compare against `Indicators` package function results
-@test maximum(sma(x)[10:end]-ma[10:end,1]) < tol
-@test maximum(ema(x)[10:end]-ma[10:end,2]) < tol
-@test maximum(wma(x)[10:end]-ma[10:end,3]) < tol
-@test maximum(hma(x)[23:end]-ma[23:end,4]) < tol
+x = cumsum(randn(n)) + x0
+ohlc = cumsum(randn(n,4)) + x0
+hlc = ohlc[:,2:3]
+
+# moving average functions
+@test size(mama(x), 1) == n
+@test size(mama(x), 2) == 2
+
+@test size(sma(x), 1) == n
+@test size(sma(x), 2) == 1
+
+@test size(ema(x), 1) == n
+@test size(ema(x), 2) == 1
+
+@test size(wma(x), 1) == n
+@test size(wma(x), 2) == 1
+
+@test size(hma(x), 1) == n
+@test size(hma(x), 2) == 1
+
+@test size(trima(x), 1) == n
+@test size(trima(x), 2) == 1
+
+@test size(mma(x), 1) == n
+@test size(mma(x), 2) == 1
+
+@test size(tema(x), 1) == n
+@test size(tema(x), 2) == 1
+
+@test size(dema(x), 1) == n
+@test size(dema(x), 2) == 1
+
+@test size(swma(x), 1) == n
+@test size(swma(x), 2) == 1
+
+@test size(kama(x), 1) == n
+@test size(kama(x), 2) == 1
+
+@test size(alma(x), 1) == n
+@test size(alma(x), 2) == 1
+
+@test size(zlema(x), 1) == n
+@test size(zlema(x), 2) == 1
+
